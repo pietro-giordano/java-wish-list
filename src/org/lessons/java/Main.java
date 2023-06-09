0ac8011dd6c43fe7c9df90480e08bc95c70a2bb6
Pietro Giordano <giopi87@gmail.com>
2023-06-09 15:44:52 +0200

    fine bonus primo esercizio

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -1,16 +1,36 @@
 package org.lessons.java;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         System.out.println("Benvenuto nella tua lista dei regali!");
         List<String> gifts = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
 
+        File newFile = new File("./gifts.txt");
+        if (newFile.exists()){
+            System.out.println("Lista regali già presente");
+            try {
+                Scanner reader = new Scanner(newFile);
+                while (reader.hasNextLine()) {
+                    gifts.add(reader.nextLine());
+                }
+                reader.close();
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            System.out.println("Nessuna lista presente");
+        }
+
+        Scanner scanner = new Scanner(System.in);
         boolean exit = false;
         do {
             System.out.println("Inserisci il nome del regalo che vuoi inserire in lista o scrivi 'esci' per chiudere:");
@@ -38,8 +58,34 @@ public class Main {
             }
         } while (!exit);
 
+        scanner.close();
+
         Collections.sort(gifts);
         System.out.println("Questa è la lista ordinata:");
         System.out.println(gifts);
+
+        // bonus file
+        if (!newFile.exists()) {
+            try {
+                if (newFile.createNewFile()) {
+                    System.out.println("File creato");
+                } else {
+                    System.out.println("File non creato");
+                }
+            } catch (IOException e) {
+                System.out.println("Errore in fase di creazione");
+            }
+        }
+
+        try {
+            FileWriter writer = new FileWriter("gifts.txt");
+            for (String gift : gifts) {
+                writer.write(gift.toString() + "\n");
+            }
+            writer.close();
+            System.out.println("Scrittura su file avvenuta correttamente");
+        } catch (IOException e) {
+            System.out.println("Errore scrittura su file");
+        }
     }
 }
